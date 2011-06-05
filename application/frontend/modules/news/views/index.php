a62f813b376c313172d6a989c522f84c090d8807
vahid <vahid@vahid.(none)>
2011-06-05 21:57:04 +0430

    some change on display news

--- application/frontend/modules/news/views/index.php
+++ application/frontend/modules/news/views/index.php
@@ -1,1 +1,12 @@
-<p>hello <?php echo $name?></p>
+<table>
+	<tr>
+		<td>ID</td>
+		<td>Title</td>
+	</tr>
+<?php while($row = mysql_fetch_assoc($news)):?>
+	<tr>
+		<td><?php echo $row['id']?></td>
+		<td><?php echo $row['title']?></td>
+	</tr>
+<?php endwhile;?>
+</table>
