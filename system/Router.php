ad6b80e2262d0e77f98383777ce2ecf415b29aca
vahid <vahid@vahid.(none)>
2011-06-06 05:35:31 +0430

    get dynamic module and action and other parameters from URI

--- system/Router.php
+++ system/Router.php
@@ -3,9 +3,23 @@ class Router
 {
 	var $action = 'index';
 	var $module = 'news';
+	var $params = array();
 	
 	function __construct()
 	{
+		$this->init();
+	}
+	
+	function init()
+	{
+		$uri = $_SERVER['PATH_INFO'];
+		$uri_array = explode('/', $uri);
+		array_shift($uri_array);
+		
+		$this->module = $uri_array[0];
+		$this->action = $uri_array[1];
+		
+		$this->params = array_slice($uri_array, 2);
 		
 	}
 }
