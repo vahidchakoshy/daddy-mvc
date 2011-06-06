d89c2613866e5f847b36cafae1f9a0a5c5e629fe
vahid <vahid@vahid.(none)>
2011-06-06 05:37:54 +0430

    add simple error log

--- system/BaseActions.php
+++ system/BaseActions.php
@@ -2,32 +2,39 @@
 class BaseActions
 {
 	var $parameters = array();
-	
+
 	var $app;
-	
+
 	function __construct()
 	{
 		$this->app = APPLICATION::getInsetance();
 	}
-	
+
 	function __set($key, $value)
 	{
 		$this->parameters[$key] = $value;
 	}
-	
+
 	function __get($key)
 	{
 		return $this->parameters[$key];
 	}
-	
+
 	function __destruct()
 	{
 		$app = $this->app;
+
 		extract($this->parameters);
 		ob_start();
-		require 'application/'.APPLICATION.'/modules/'.$app->Router->module.'/views/'.$app->Router->action.'.php';
-		
+		if(file_exists('application/'.APPLICATION.'/modules/'.$app->Router->module.'/views/'.$app->Router->action.'.php'))
+		{
+			require 'application/'.APPLICATION.'/modules/'.$app->Router->module.'/views/'.$app->Router->action.'.php';
+		}else{
+			echo 'we dont have view file for this action';
+		}
+			
 		$this->app->Template->setVar('content', ob_get_contents());
 		@ob_end_clean();
+
 	}
 }
