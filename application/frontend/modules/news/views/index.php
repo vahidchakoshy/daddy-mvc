519c0bc77ef24586c9adbf8ae63b149f06752445
vahid <vahid@vahid.(none)>
2011-06-07 01:16:12 +0430

    some change and using url helper in news index view

--- application/frontend/modules/news/views/index.php
+++ application/frontend/modules/news/views/index.php
@@ -2,11 +2,13 @@
 	<tr>
 		<td>ID</td>
 		<td>Title</td>
+		<td>Manage</td>
 	</tr>
 <?php while($row = mysql_fetch_assoc($news)):?>
 	<tr>
 		<td><?php echo $row['id']?></td>
 		<td><?php echo $row['title']?></td>
+		<td><a href="<?php echo base_url() ?>index.php/news/view/<?php echo $row['id']?>">نمایش</a></td>
 	</tr>
 <?php endwhile;?>
 </table>
