ac85c8d28320149abfdd6052ecf67a73be610ce7
vahid <vahid@vahid.(none)>
2011-06-08 00:52:21 +0430

    fix some bug on router class

--- system/Router.php
+++ system/Router.php
@@ -14,21 +14,26 @@ class Router
 	{
 		$this->module = Config::get('router', 'default_module');
 		$this->action = Config::get('router', 'default_action');
-		
+
 		if(isset($_SERVER['PATH_INFO']))
 		{
 			$uri = $_SERVER['PATH_INFO'];
 			$uri_array = explode('/', $uri);
 			array_shift($uri_array);
-
-			$this->module = $uri_array[0];
-			
-			if(isset($uri_array[1]))
+			if(count($uri_array) > 0)
 			{
-				$this->action = $uri_array[1];
-			}
+				if($uri_array[0] != '')
+				{
+					$this->module = $uri_array[0];
+				}
+					
+				if(isset($uri_array[1]) && $uri_array[1] != '')
+				{
+					$this->action = $uri_array[1];
+				}
 
-			$this->params = array_slice($uri_array, 2);
+				$this->params = array_slice($uri_array, 2);
+			}
 		}
 
 	}
